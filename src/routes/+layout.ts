5278da476711c1156646214704ec4ac5b46e1075
Nicolas <nicolaspohren@gmail.com>
2024-01-08 23:01:25 -0300

    build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,11 @@
 import posthog from 'posthog-js'
+import { browser } from '$app/environment'
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true
 
-posthog.init('phc_C5dhvjqUeyWnOpGllNvTkBjNoxGZRcFaa5ItsyCqw2', { api_host: 'https://app.posthog.com' })
+export const load = async () => {
+    if (browser) {
+        posthog.init('phc_C5dhvjqUeyWnOpGllNvTkBjNoxGZRcFaa5ItsyCqw2', { api_host: 'https://app.posthog.com' })
+    }
+    return
+}
